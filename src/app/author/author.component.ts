642c75470fe335c7df15a6f1d852d467e0afbaf9
AlecZeng <aleczeng.nz@gmail.com>
2019-03-17 19:57:45 +1300

    button active

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -4,12 +4,18 @@ import {AuthorService} from '../service/author.service';
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
+  styleUrls: ['./author.component.css']
 })
 export class AuthorComponent {
   protected authors: any;
-  isActive: false;
+  isActive: boolean;
 
   constructor(private authorService: AuthorService) {
     this.authors = authorService.getAuthors();
+    this.isActive = true;
+  }
+
+  onClick() {
+    this.isActive = !this.isActive;
   }
 }
