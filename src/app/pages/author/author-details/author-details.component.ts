e6a094ff34939bbe8e7ebae2ebfbeac7bfc293ec
AlecZeng <aleczeng.nz@gmail.com>
2019-03-19 08:49:53 +1300

    routerlink with parameter

--- src/app/pages/author/author-details/author-details.component.ts
+++ src/app/pages/author/author-details/author-details.component.ts
@@ -9,12 +9,14 @@ import {ActivatedRoute} from '@angular/router';
 export class AuthorDetailsComponent implements OnInit {
   id: number;
 
+
   constructor(public route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.id = +params.get('id');
+      console.log(this.id);
     });
   }
 }
