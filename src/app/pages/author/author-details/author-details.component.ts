d87b6ea24ec9ed627fac45c2fa955bb6328c0da7
AlecZeng <aleczeng.nz@gmail.com>
2019-03-18 18:26:00 +1300

    route ActivatedRoute paraMap

--- src/app/pages/author/author-details/author-details.component.ts
+++ src/app/pages/author/author-details/author-details.component.ts
@@ -1,11 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-author-details',
   templateUrl: './author-details.component.html',
   styleUrls: ['./author-details.component.css']
 })
-export class AuthorDetailsComponent {
-  constructor() {
+export class AuthorDetailsComponent implements OnInit {
+  id: number;
+
+  constructor(public route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = +params.get('id');
+    });
   }
 }
