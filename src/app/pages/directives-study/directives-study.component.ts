80d55d9c59ec09692e2aaedde8044bcbfa4f46d3
AlecZeng <zxh93321@gmail.com>
2019-03-20 20:17:52 +1300

    ngSwitchCase

--- src/app/pages/directives-study/directives-study.component.ts
+++ src/app/pages/directives-study/directives-study.component.ts
@@ -5,8 +5,8 @@ import {Component} from '@angular/core';
   templateUrl: './directives-study.component.html',
 })
 export class DirectivesStudyComponent {
+  viewSelected: string;
 
   constructor() {
   }
-
 }
