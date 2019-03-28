8e7a1370a68b25663a262f591e5e087986f0e181
AlecZeng <zxh93321@gmail.com>
2019-03-29 12:35:48 +1300

    form array

--- src/app/shared/signup-form/signup-form.component.ts
+++ src/app/shared/signup-form/signup-form.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ValidatorsPattern} from '../../common/validators/validators.pattern';
 
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
+  styleUrls: ['signup-form.component.css']
 })
 export class SignupFormComponent {
 
@@ -17,14 +18,29 @@ export class SignupFormComponent {
         ValidatorsPattern.cannotContainSpace
       ], ValidatorsPattern.shouldBeUnique),
       password: new FormControl('', Validators.required)
-    })
+    }),
+
+    topics: new FormArray([])
   });
 
   get username() {
     return this.form.get('username');
   }
 
+  get topics() {
+    return <FormArray>this.form.get('topics');
+  }
+
   login() {
     this.form.setErrors({formInvalid: true});
+  }
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+  }
+
+  onDelete(topic: FormControl) {
+    const index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
   }
 }
