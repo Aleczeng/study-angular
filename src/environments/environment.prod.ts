6387c4441135e3171265ecc427a0a7725f6e1ce5
AlecZeng <zxh93321@gmail.com>
2019-04-16 19:41:57 +1200

    gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyDq2-NohDoGKnfAsMnd_ZYeYO1Fx64KDA0',
-    authDomain: 'study-angular-9bb6f.firebaseapp.com',
-    databaseURL: 'https://study-angular-9bb6f.firebaseio.com',
-    projectId: 'study-angular-9bb6f',
-    storageBucket: 'study-angular-9bb6f.appspot.com',
-    messagingSenderId: '285944149250'
-  }
 };
