982d33d8302e7e63ad21c96f925f90f7d39c648c
AlecZeng <zxh93321@gmail.com>
2019-04-16 19:40:18 +1200

    gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyDq2-NohDoGKnfAsMnd_ZYeYO1Fx64KDA0',
-    authDomain: 'study-angular-9bb6f.firebaseapp.com',
-    databaseURL: 'https://study-angular-9bb6f.firebaseio.com',
-    projectId: 'study-angular-9bb6f',
-    storageBucket: 'study-angular-9bb6f.appspot.com',
-    messagingSenderId: '285944149250'
-  }
 };
 
 /*
