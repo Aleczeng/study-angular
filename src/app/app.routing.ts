df2988f43815c69c92c1704e88e863bbb1f00ee7
AlecZeng <zxh93321@gmail.com>
2019-05-12 19:02:02 +1200

    fix bug

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,5 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {AuthorListComponent} from './pages/author/author-list/author-list.component';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
-import {AuthorDetailsComponent} from './pages/author/author-details/author-details.component';
-import {DataBindingComponent} from './pages/data-binding/data-binding.component';
-import {PipesComponent} from './pages/pipes-example/pipes.component';
-import {ReuseComponentComponent} from './pages/reuse-component/reuse-component.component';
-import {DirectivesStudyComponent} from './pages/directives-study/directives-study.component';
-import {FormsStudyComponent} from './pages/forms-study/forms-study.component';
-import {HttpStudyComponent} from './pages/http-study/http-study.component';
-import {HttpExerciseComponent} from './pages/http-exercise/http-exercise.component';
-import {FirebaseStudyComponent} from './pages/firebase-study/firebase-study.component';
 import {LoginComponent} from './pages/login/login.component';
 
 
--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -12,6 +12,7 @@ export class NavBarComponent {
 
   constructor(public authService: AuthService,
               public router: Router) {
+    this.isLogin = this.authService.isAuthenticated();
     this.authService.isLogin.subscribe(isLogin => {
       this.isLogin = isLogin;
       console.log(this.isLogin);
